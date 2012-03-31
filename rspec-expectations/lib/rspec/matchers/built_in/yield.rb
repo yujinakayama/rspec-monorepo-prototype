9189e851fcd3609ff86e21476b49bac16c2a7949
Myron Marston <myron.marston@gmail.com>
2012-03-30 18:44:21 -0700

    [expectations] Remove diffability from yield matchers for now.
    I'm not sure it makes sense given that they don't match strictly but instead match using === or ==.

---
+++ rspec-expectations/lib/rspec/matchers/built_in/yield.rb
@@ -0,0 +1,145 @@
+module RSpec
+  module Matchers
+    module BuiltIn
+      class YieldControl
+        def matches?(block)
+          yielded = false
+          block.call(lambda { |*| yielded = true })
+          yielded
+        end
+
+        def failure_message_for_should
+          "expected given block to yield control"
+        end
+
+        def failure_message_for_should_not
+          "expected given block not to yield control"
+        end
+      end
+
+      class YieldWithNoArgs
+        def matches?(block)
+          @yielded, @args = false, nil
+          block.call(lambda { |*a| @yielded = true; @args = a })
+          @yielded && @args.none?
+        end
+
+        def failure_message_for_should
+          "expected given block to yield with no arguments, but #{failure_reason}"
+        end
+
+        def failure_message_for_should_not
+          "expected given block not to yield with no arguments, but did"
+        end
+
+      private
+
+        def failure_reason
+          if !@yielded
+            "did not yield"
+          else
+            "yielded with arguments: #{@args.inspect}"
+          end
+        end
+      end
+
+      class YieldWithArgs
+        def initialize(*args)
+          @expected = args
+        end
+
+        def matches?(block)
+          @yielded, @actual = false, nil
+          block.call(lambda { |*a| @yielded = true; @actual = a })
+          @yielded && args_match?
+        end
+
+        def failure_message_for_should
+          "expected given block to yield with arguments, but #{positive_failure_reason}"
+        end
+
+        def failure_message_for_should_not
+          "expected given block not to yield with arguments, but #{negative_failure_reason}"
+        end
+
+      private
+
+        def positive_failure_reason
+          if !@yielded
+            "did not yield"
+          else
+            @positive_args_failure
+          end
+        end
+
+        def negative_failure_reason
+          if all_args_match?
+            "yielded with expected arguments" +
+              "\nexpected not: #{@expected.inspect}" +
+              "\n         got: #{@actual.inspect} (compared using === and ==)"
+          else
+            "did"
+          end
+        end
+
+        def args_match?
+          if @expected.none? # expect {...}.to yield_with_args
+            @positive_args_failure = "yielded with no arguments" if @actual.none?
+            return @actual.any?
+          end
+
+          unless match = all_args_match?
+            @positive_args_failure = "yielded with unexpected arguments" +
+              "\nexpected: #{@expected.inspect}" +
+              "\n     got: #{@actual.inspect} (compared using === and ==)"
+          end
+
+          match
+        end
+
+        def all_args_match?
+          return false if @expected.size != @actual.size
+
+          @expected.zip(@actual).all? do |expected, actual|
+            expected === actual || actual == expected
+          end
+        end
+      end
+
+      class YieldSuccessiveArgs
+        def initialize(*args)
+          @expected = args
+          @actual   = []
+        end
+
+        def matches?(block)
+          block.call(lambda { |a| @actual << a })
+          args_match?
+        end
+
+        def failure_message_for_should
+          "expected given block to yield successively with arguments, but yielded with unexpected arguments" +
+            "\nexpected: #{@expected.inspect}" +
+            "\n     got: #{@actual.inspect} (compared using === and ==)"
+        end
+
+        def failure_message_for_should_not
+          "expected given block not to yield successively with arguments, but yielded with expected arguments" +
+              "\nexpected not: #{@expected.inspect}" +
+              "\n         got: #{@actual.inspect} (compared using === and ==)"
+        end
+
+      private
+
+        def args_match?
+          return false if @expected.size != @actual.size
+
+          @expected.zip(@actual).all? do |expected, actual|
+            expected === actual || actual == expected
+          end
+        end
+      end
+    end
+  end
+end
+
