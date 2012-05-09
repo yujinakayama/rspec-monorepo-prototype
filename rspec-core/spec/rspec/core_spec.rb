d78077788f71fa0d134d963b0e941279eba7ee40
David Chelimsky <dchelimsky@gmail.com>
2012-05-08 21:38:28 -0500

    [mocks] Expose ArgumentListMatcher as a formal API
    - supports use by 3rd party mock frameworks like Surrogate.

--- rspec-core/spec/rspec/core_spec.rb
+++ rspec-core/spec/rspec/core_spec.rb
@@ -1,21 +1,25 @@
 require 'spec_helper'
 
-describe "RSpec.configuration" do
-  it "returns the same object every time" do
-    RSpec.configuration.should equal(RSpec.configuration)
-  end
-end
+describe RSpec::Core do
 
-describe "RSpec.configure" do
-  it "yields the current configuration" do
-    RSpec.configure do |config|
-      config.should eq(RSpec::configuration)
+  describe "#configuration" do
+    it "returns the same object every time" do
+      RSpec.configuration.should equal(RSpec.configuration)
     end
   end
-end
 
-describe "RSpec.world" do
-  it "returns the RSpec::Core::World instance the current run is using" do
-    RSpec.world.should be_instance_of(RSpec::Core::World)
+  describe "#configure" do
+    it "yields the current configuration" do
+      RSpec.configure do |config|
+        config.should eq(RSpec::configuration)
+      end
+    end
   end
+
+  describe "#world" do
+    it "returns the RSpec::Core::World instance the current run is using" do
+      RSpec.world.should be_instance_of(RSpec::Core::World)
+    end
+  end
+
 end
