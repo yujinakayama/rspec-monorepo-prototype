488294b5248cae9c7a294c751bfa98a78dc00af4
Jon Rowe <hello@jonrowe.co.uk>
2013-04-13 21:56:53 +1000

    [core] properly stub deprecation warnings

---
+++ rspec-core/lib/rspec/core/deprecation.rb
@@ -0,0 +1,43 @@
+module RSpec
+  class << self
+    # @private
+    #
+    # Used internally to print deprecation warnings
+    def deprecate(method, alternate_method=nil, version=nil)
+      lines = ["#{method} is deprecated"]
+      if version
+        lines.last << ", and will be removed from rspec-#{version}"
+      end
+      if alternate_method
+        lines << "use #{alternate_method} instead"
+      end
+
+      lines << "called from #{caller(0)[2]}"
+
+      warn_deprecation "\n" + lines.map {|l| "DEPRECATION: #{l}"}.join("\n") + "\n"
+    end
+
+    # @private
+    #
+    # Used internally to print deprecation warnings
+    def warn_deprecation(message)
+      RSpec.configuration.deprecation_io.puts(message)
+    end
+
+    # @private
+    #
+    # Used internally to print the count of deprecation warnings
+    def deprecations?
+      RSpec.configuration.deprecation_io.deprecations > 0
+    end
+
+    # @private
+    #
+    # Used internally to print deprecation summary
+    def deprecations_summary
+      io = RSpec.configuration.deprecation_io
+      "There were #{io.deprecations} deprecations logged to #{io.description}"
+    end
+
+  end
+end
---
+++ rspec-core/spec/rspec/core/deprecations_spec.rb
@@ -0,0 +1,93 @@
+require "spec_helper"
+
+describe "support for deprecation warnings" do
+  it "includes the method to deprecate" do
+    expect(RSpec).to receive(:warn).with(/^DEPRECATION: deprecated_method/)
+    RSpec.deprecate("deprecated_method")
+  end
+
+  it "includes the replacement when provided" do
+    expect(RSpec).to receive(:warn).with(/deprecated_method.*\nDEPRECATION:.*replacement/m)
+    RSpec.deprecate("deprecated_method", "replacement")
+  end
+
+  it "includes the version number when provided" do
+    expect(RSpec).to receive(:warn).with(/deprecated_method.*rspec-37\.0\.0\nDEPRECATION:.*replacement/m)
+    RSpec.deprecate("deprecated_method", "replacement", "37.0.0")
+  end
+
+end
+
+describe "deprecated methods" do
+  describe "Spec" do
+    it "is deprecated" do
+      RSpec.should_receive(:warn_deprecation).with(/Spec .* RSpec/i)
+      Spec
+    end
+
+    it "returns RSpec" do
+      RSpec.stub(:warn_deprecation)
+      expect(Spec).to eq(RSpec)
+    end
+
+    it "doesn't include backward compatibility in const_missing backtrace" do
+      RSpec.stub(:warn_deprecation)
+      exception = nil
+      begin
+        ConstantThatDoesNotExist
+      rescue Exception => exception
+      end
+      expect(exception.backtrace.find { |l| l =~ /lib\/rspec\/core\/backward_compatibility/ }).to be_nil
+    end
+  end
+
+  describe RSpec::Core::ExampleGroup do
+    describe 'running_example' do
+      it 'is deprecated' do
+        RSpec.should_receive(:warn_deprecation)
+        self.running_example
+      end
+
+      it "delegates to example" do
+        RSpec.stub(:warn_deprecation)
+        expect(running_example).to eq(example)
+      end
+    end
+  end
+
+  describe RSpec::Core::SharedExampleGroup do
+    describe 'share_as' do
+      it 'is deprecated' do
+        RSpec.should_receive(:warn_deprecation)
+        RSpec::Core::SharedExampleGroup.share_as(:DeprecatedSharedConst) {}
+      end
+    end
+  end
+
+  describe "Spec::Runner.configure" do
+    it "is deprecated" do
+      RSpec.stub(:warn_deprecation)
+      RSpec.should_receive(:deprecate)
+      Spec::Runner.configure
+    end
+  end
+
+  describe "Spec::Rake::SpecTask" do
+    it "is deprecated" do
+      RSpec.stub(:warn_deprecation)
+      RSpec.should_receive(:deprecate)
+      Spec::Rake::SpecTask
+    end
+
+    it "doesn't include backward compatibility in const_missing backtrace" do
+      RSpec.stub(:warn_deprecation)
+      exception = nil
+      begin
+        Spec::Rake::ConstantThatDoesNotExist
+      rescue Exception => exception
+      end
+      expect(exception.backtrace.find { |l| l =~ /lib\/rspec\/core\/backward_compatibility/ }).to be_nil
+    end
+  end
+
+end
