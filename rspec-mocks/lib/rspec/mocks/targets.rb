436fbd058543b3d8b880fe1f31a2631e20f9134a
Jon Rowe <hello@jonrowe.co.uk>
2013-09-12 20:49:19 +1000

    [core] rework spec to harness output correctly

--- rspec-mocks/lib/rspec/mocks/targets.rb
+++ rspec-mocks/lib/rspec/mocks/targets.rb
@@ -8,50 +8,27 @@ module RSpec
         @target = target
       end
 
-      def self.delegate_to(matcher_method)
-        define_method(:to) do |matcher, &block|
-          unless Matchers::Receive === matcher || Matchers::ReceiveMessages === matcher
-            raise_unsupported_matcher(:to, matcher)
-          end
-          define_matcher(matcher, matcher_method, &block)
-        end
-      end
-
-      def self.delegate_not_to(matcher_method, options = {})
-        method_name = options.fetch(:from)
+      def self.delegate_to(matcher_method, options = {})
+        method_name = options.fetch(:from) { :to }
         define_method(method_name) do |matcher, &block|
-          case matcher
-          when Matchers::Receive         then define_matcher(matcher, matcher_method, &block)
-          when Matchers::ReceiveMessages then raise_negation_unsupported(method_name, matcher)
-          else
-            raise_unsupported_matcher(method_name, matcher)
+          unless Matchers::Receive === matcher
+            raise UnsupportedMatcherError, "only the `receive` matcher is supported " +
+              "with `#{expression}(...).#{method_name}`, but you have provided: #{matcher}"
           end
+
+          matcher.__send__(matcher_method, @target, &block)
         end
       end
 
-      def self.disallow_negation(method_name)
-        define_method(method_name) do |matcher, *args|
-          raise_negation_unsupported(method_name, matcher)
+      def self.disallow_negation(method)
+        define_method method do |*args|
+          raise NegationUnsupportedError,
+            "`#{expression}(...).#{method} receive` is not supported since it " +
+            "doesn't really make sense. What would it even mean?"
         end
       end
 
     private
-
-      def define_matcher(matcher, name, &block)
-        matcher.__send__(name, @target, &block)
-      end
-
-      def raise_unsupported_matcher(method_name, matcher)
-        raise UnsupportedMatcherError,
-          "only the `receive` or `receive_messages` matchers are supported " +
-          "with `#{expression}(...).#{method_name}`, but you have provided: #{matcher}"
-      end
-
-      def raise_negation_unsupported(method_name, matcher)
-        raise NegationUnsupportedError,
-          "`#{expression}(...).#{method_name} #{matcher.name}` is not supported since it " +
-          "doesn't really make sense. What would it even mean?"
-      end
 
       def expression
         self.class::EXPRESSION
@@ -68,12 +45,12 @@ module RSpec
     class ExpectationTarget < TargetBase
       EXPRESSION = :expect
       delegate_to :setup_expectation
-      delegate_not_to :setup_negative_expectation, :from => :not_to
-      delegate_not_to :setup_negative_expectation, :from => :to_not
+      delegate_to :setup_negative_expectation, :from => :not_to
+      delegate_to :setup_negative_expectation, :from => :to_not
     end
 
     class AnyInstanceAllowanceTarget < TargetBase
-      EXPRESSION = :allow_any_instance_of
+      EXPRESSION = :expect_any_instance_of
       delegate_to :setup_any_instance_allowance
       disallow_negation :not_to
       disallow_negation :to_not
@@ -82,8 +59,8 @@ module RSpec
     class AnyInstanceExpectationTarget < TargetBase
       EXPRESSION = :expect_any_instance_of
       delegate_to :setup_any_instance_expectation
-      delegate_not_to :setup_any_instance_negative_expectation, :from => :not_to
-      delegate_not_to :setup_any_instance_negative_expectation, :from => :to_not
+      delegate_to :setup_any_instance_negative_expectation, :from => :not_to
+      delegate_to :setup_any_instance_negative_expectation, :from => :to_not
     end
   end
 end
