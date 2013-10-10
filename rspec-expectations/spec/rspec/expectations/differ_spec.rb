3f3647b0a792c925d9e534c43908c116e29270bb
Jon Rowe <hello@jonrowe.co.uk>
2013-10-11 09:10:06 +1100

    [core] Better changelog entry for #1099

--- rspec-expectations/spec/rspec/expectations/differ_spec.rb
+++ rspec-expectations/spec/rspec/expectations/differ_spec.rb
@@ -54,10 +54,15 @@ EOD
             @actual="Tu avec carte {count} item has".encode('UTF-16LE')
             expect(subject).to eql 'Could not produce a diff because of the encoding of the string (UTF-16LE)'
           end
-          it 'ouputs a message when encountering differently encoded strings' do
+          it 'outputs a message when encountering differently encoded strings' do
             @expected="Tu avec carté {count} itém has".encode('UTF-16LE')
             @actual="Tu avec carte {count} item has"
             expect(subject).to eql 'Could not produce a diff because the encoding of the actual string (UTF-8) differs from the encoding of the expected string (UTF-16LE)'
+          end
+          it "handles conversions that aren't possible" do
+            @expected = "강인철".encode('UTF-8')
+            @actual   = "abc".encode('us-ascii')
+            expect(subject).to eql "\n@@ -1,2 +1,2 @@\n-abc\n+???\n"
           end
         end
       end
