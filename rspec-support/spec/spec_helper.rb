609cfcaa210167fdc264b58eb57b6862fc6db4e6
Jon Rowe <hello@jonrowe.co.uk>
2013-12-01 18:08:54 +1100

    [support] allow warn_with to take replacement as an argument

--- rspec-support/spec/spec_helper.rb
+++ rspec-support/spec/spec_helper.rb
@@ -1,2 +1,11 @@
+if ENV['TRAVIS']
+  require 'simplecov' if RUBY_VERSION.to_f > 1.8
+  require 'coveralls'
+  Coveralls.wear! do
+    add_filter '/bundle/'
+    add_filter '/spec/'
+    add_filter '/tmp/'
+  end
+end
+
 require 'rspec/support/spec'
-RSpec::Support::Spec.setup_simplecov
