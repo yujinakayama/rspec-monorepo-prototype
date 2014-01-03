rspec/rspec-metagem

commit ec935403fbe8324dbc3bd9136edbb49c39306854

`contain_exactly` should work with sets and other collections, too.

lib/rspec/matchers/built_in/contain_exactly.rb
@@ -3,21 +3,21 @@ module RSpec
     module BuiltIn
       class ContainExactly < BaseMatcher
         def match(expected, actual)
-          return false unless actual.respond_to? :to_ary
+          return false unless actual_is_a_collection?
+          @actual = actual.to_a
           extra_items.empty? && missing_items.empty?
         end
 
         def failure_message
-          if actual.respond_to? :to_ary
-            message =  "expected collection contained:  #{safe_sort(surface_descriptions_in expected).inspect}\n"
+          if actual_is_a_collection?
+            message  = "expected collection contained:  #{safe_sort(surface_descriptions_in expected).inspect}\n"
             message += "actual collection contained:    #{safe_sort(actual).inspect}\n"
             message += "the missing elements were:      #{safe_sort(surface_descriptions_in missing_items).inspect}\n" unless missing_items.empty?
             message += "the extra elements were:        #{safe_sort(extra_items).inspect}\n" unless extra_items.empty?
+            message
           else
-            message = "expected an array, actual collection was #{actual.inspect}"
+            "expected a collection that can be converted to an array with `#to_a`, but got #{actual.inspect}"
           end
-
-          message
         end
 
         def failure_message_when_negated
@@ -29,6 +29,10 @@ module RSpec
         end
 
       private
+
+        def actual_is_a_collection?
+          enumerable?(actual) && actual.respond_to?(:to_a)
+        end
 
         def safe_sort(array)
           array.sort rescue array

spec/rspec/matchers/built_in/contain_exactly_spec.rb
@@ -189,27 +189,26 @@ describe "matching against things that aren't arrays" do
   it "fails with nil and the expected error message is given" do
     expect {
       expect(nil).to contain_exactly(1, 2, 3)
-    }.to fail_with(/expected an array/)
+    }.to fail_with(/expected a collection/)
   end
 
   it "fails with a float and the expected error message is given" do
     expect {
       expect(3.7).to contain_exactly(1, 2, 3)
-    }.to fail_with(/expected an array/)
+    }.to fail_with(/expected a collection/)
   end
 
   it "fails with a string and the expected error message is given" do
     expect {
       expect("I like turtles").to contain_exactly(1, 2, 3)
-    }.to fail_with(/expected an array/)
+    }.to fail_with(/expected a collection/)
   end
 
-  context "when using the `should =~` syntax", :uses_should do
-    it 'fails with a clear message when given a hash' do
-      expect {
-        {}.should =~ {}
-      }.to fail_with(/expected an array/)
-    end
+  it 'works with other collection types' do
+    expect(Set.new([3, 2, 1])).to contain_exactly(1, 2, 3)
+    expect {
+      expect(Set.new([3, 2, 1])).to contain_exactly(1, 2)
+    }.to fail_matching("expected collection contained:  [1, 2]")
   end
 end
 

yujinakayama/rspec-monorepo-prototype

commit 601d28590add42449566de57403ed28bd7752fee

[expectations] Fix AR::Relation regression in ec935403fbe8324dbc3bd9136edbb49c39306854.
AR::Relation implements `to_ary` but is not Enumerable.

rspec-expectations/lib/rspec/matchers/built_in/contain_exactly.rb
@@ -3,20 +3,20 @@ module RSpec
     module BuiltIn
       class ContainExactly < BaseMatcher
         def match(expected, actual)
-          return false unless actual_is_a_collection?
-          @actual = actual.to_a
+          convert_actual_to_an_array or return false
           match_when_sorted? || (extra_items.empty? && missing_items.empty?)
         end
 
         def failure_message
-          if actual_is_a_collection?
+          if Array === actual
             message  = "expected collection contained:  #{safe_sort(surface_descriptions_in expected).inspect}\n"
             message += "actual collection contained:    #{safe_sort(actual).inspect}\n"
             message += "the missing elements were:      #{safe_sort(surface_descriptions_in missing_items).inspect}\n" unless missing_items.empty?
             message += "the extra elements were:        #{safe_sort(extra_items).inspect}\n" unless extra_items.empty?
             message
           else
-            "expected a collection that can be converted to an array with `#to_a`, but got #{actual.inspect}"
+            "expected a collection that can be converted to an array with " +
+            "`#to_ary` or `#to_a`, but got #{actual.inspect}"
           end
         end
 
@@ -38,8 +38,14 @@ module RSpec
           values_match?(safe_sort(expected), safe_sort(actual))
         end
 
-        def actual_is_a_collection?
-          enumerable?(actual) && actual.respond_to?(:to_a)
+        def convert_actual_to_an_array
+          if actual.respond_to?(:to_ary)
+            @actual = actual.to_ary
+          elsif enumerable?(actual) && actual.respond_to?(:to_a)
+            @actual = actual.to_a
+          else
+            return false
+          end
         end
 
         def safe_sort(array)

rspec-expectations/spec/rspec/matchers/built_in/contain_exactly_spec.rb
@@ -15,6 +15,18 @@ class UnsortableObject
   end
 end
 
+# AR::Relation is meant to act like a collection, but does
+# not include `Enumerable`. It does implement `to_ary`.
+class FakeActiveRecordRelation
+  def initialize(records)
+    @records = records
+  end
+
+  def to_ary
+    @records
+  end
+end
+
 describe "should =~ array", :uses_should do
   it "passes a valid positive expectation" do
     [1, 2].should =~ [2, 1]
@@ -208,6 +220,14 @@ describe "matching against things that aren't arrays" do
     expect(Set.new([3, 2, 1])).to contain_exactly(1, 2, 3)
     expect {
       expect(Set.new([3, 2, 1])).to contain_exactly(1, 2)
+    }.to fail_matching("expected collection contained:  [1, 2]")
+  end
+
+  it 'works with non-enumerables that implement `to_ary`' do
+    relation = FakeActiveRecordRelation.new([1, 2, 3])
+    expect(relation).to contain_exactly(2, 1, 3)
+    expect {
+      expect(relation).to contain_exactly(1, 2)
     }.to fail_matching("expected collection contained:  [1, 2]")
   end
 end