87583957c3ef6df4a5c9c34ddc0524ec3199f94b
Myron Marston <myron.marston@gmail.com>
2014-01-06 20:55:26 -0800

    [mocks] Clean up expectation ordering.
    - It was duplicated in Proxy.
    - Makes more sense to initialize it in Space init.

--- rspec-expectations/lib/rspec/matchers/built_in/be_between.rb
+++ rspec-expectations/lib/rspec/matchers/built_in/be_between.rb
@@ -4,11 +4,22 @@ module RSpec
       class BeBetween < BaseMatcher
         def initialize(min, max)
           @min, @max = min, max
+
+          @less_than_operator = :<=
+          @greater_than_operator = :>=
+          @mode = :inclusive
+        end
+
+        def exclusive
+          @less_than_operator = :<
+          @greater_than_operator = :>
+          @mode = :exclusive
+          self
         end
 
         def matches?(actual)
           @actual = actual
-          comparable? and @actual.between?(@min, @max)
+          comparable? and compare
         rescue ArgumentError
           false
         end
@@ -18,17 +29,21 @@ module RSpec
         end
 
         def description
-          "be between #{@min.inspect} and #{@max.inspect} (inclusive)"
+          "be between #{@min.inspect} and #{@max.inspect} (#{@mode})"
         end
 
       private
 
         def comparable?
-          @actual.respond_to?(:between?)
+          @actual.respond_to?(@less_than_operator) and @actual.respond_to?(@greater_than_operator)
         end
 
         def not_comparable_clause
-          ", but it does not respond to `between?`" unless comparable?
+          ", but it does not respond to `#{@less_than_operator}` and `#{@greater_than_operator}`" unless comparable?
+        end
+
+        def compare
+          @actual.send(@greater_than_operator, @min) and @actual.send(@less_than_operator, @max)
         end
       end
     end
