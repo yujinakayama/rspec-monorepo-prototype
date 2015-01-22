36ae37f874d140dd0a7f391b487e8e40f26201d4
Myron Marston <myron.marston@gmail.com>
2015-01-22 08:24:22 -0800

    [expectations] Doc improvements.

--- rspec-expectations/lib/rspec/matchers/built_in/start_and_end_with.rb
+++ rspec-expectations/lib/rspec/matchers/built_in/start_and_end_with.rb
@@ -4,7 +4,7 @@ module RSpec
       # @api private
       # Base class for the `end_with` and `start_with` matchers.
       # Not intended to be instantiated directly.
-      class StartOrEndWith < BaseMatcher
+      class StartAndEndWith < BaseMatcher
         def initialize(*expected)
           @actual_does_not_have_ordered_elements = false
           @expected = expected.length == 1 ? expected.first : expected
@@ -43,6 +43,10 @@ module RSpec
           end
         end
 
+        def actual_is_unordered
+          ArgumentError.new("#{actual.inspect} does not have ordered elements")
+        end
+
         def subsets_comparable?
           # Structs support the Enumerable interface but don't really have
           # the semantics of a subset of a larger set...
@@ -52,16 +56,10 @@ module RSpec
         end
       end
 
-      # For RSpec 3.1, the base class was named `StartAndEndWith`. For SemVer reasons,
-      # we still provide this constant until 4.0.
-      # @deprecated Use StartOrEndWith instead.
-      # @private
-      StartAndEndWith = StartOrEndWith
-
       # @api private
       # Provides the implementation for `start_with`.
       # Not intended to be instantiated directly.
-      class StartWith < StartOrEndWith
+      class StartWith < StartAndEndWith
       private
 
         def subset_matches?
@@ -76,7 +74,7 @@ module RSpec
       # @api private
       # Provides the implementation for `end_with`.
       # Not intended to be instantiated directly.
-      class EndWith < StartOrEndWith
+      class EndWith < StartAndEndWith
       private
 
         def subset_matches?
