rspec/rspec-metagem

commit c0564a3928d9928b6bcc770455152825383d581a

Fill in execution_status before reporting example_finished.
Fixes #2290.

lib/rspec/core/example.rb
@@ -461,15 +461,14 @@ module RSpec
       def finish(reporter)
         pending_message = execution_result.pending_message
 
-        reporter.example_finished(self)
         if @exception
-          record_finished :failed
           execution_result.exception = @exception
+          record_finished :failed
           reporter.example_failed self
           false
         elsif pending_message
-          record_finished :pending
           execution_result.pending_message = pending_message
+          record_finished :pending
           reporter.example_pending self
           true
         else
@@ -481,6 +480,7 @@ module RSpec
 
       def record_finished(status)
         execution_result.record_finished(status, clock.now)
+        reporter.example_finished(self)
       end
 
       def run_before_example

spec/rspec/core/example_spec.rb
@@ -555,6 +555,70 @@ RSpec.describe RSpec::Core::Example, :parent_metadata => 'sample' do
     end
   end
 
+  describe "reporting example_finished" do
+    let(:reporter) { RSpec::Core::Reporter.new(RSpec::Core::Configuration.new) }
+
+    def capture_reported_execution_result_for_example(&block)
+      reporter = RSpec::Core::Reporter.new(RSpec::Core::Configuration.new)
+
+      reported_execution_result = nil
+
+      listener = double("Listener")
+      allow(listener).to receive(:example_finished) do |notification|
+        reported_execution_result = notification.example.execution_result.dup
+      end
+
+      reporter.register_listener(listener, :example_finished)
+
+      RSpec.describe do
+        it(&block)
+      end.run(reporter)
+
+      reported_execution_result
+    end
+
+    it "fills in the execution result details before reporting a passed example as finished" do
+      execution_result = capture_reported_execution_result_for_example do
+        expect(1).to eq 1
+      end
+
+      expect(execution_result).to have_attributes(
+        :status => :passed,
+        :exception => nil,
+        :finished_at => a_value_within(1).of(Time.now),
+        :run_time => a_value >= 0
+      )
+    end
+
+    it "fills in the execution result details before reporting a failed example as finished" do
+      execution_result = capture_reported_execution_result_for_example do
+        expect(1).to eq 2
+      end
+
+      expect(execution_result).to have_attributes(
+        :status => :failed,
+        :exception => RSpec::Expectations::ExpectationNotMetError,
+        :finished_at => a_value_within(1).of(Time.now),
+        :run_time => a_value >= 0
+      )
+    end
+
+    it "fills in the execution result details before reporting a pending example as finished" do
+      execution_result = capture_reported_execution_result_for_example do
+        pending "because"
+        expect(1).to eq 2
+      end
+
+      expect(execution_result).to have_attributes(
+        :status => :pending,
+        :pending_message => "because",
+        :pending_exception => RSpec::Expectations::ExpectationNotMetError,
+        :finished_at => a_value_within(1).of(Time.now),
+        :run_time => a_value >= 0
+      )
+    end
+  end
+
   describe "#pending" do
     def expect_pending_result(example)
       expect(example).to be_pending

yujinakayama/rspec-monorepo-prototype

commit 2f6e3229c179c21b07c5c710fc65968a33b58436

[core] Ensure example_finished gets reported from context-hook failures
Regression in c0564a3928d9928b6bcc770455152825383d581a

`#reporter` is a `RSpec::Core::NullReporter` at this point, so pass along
the correct `reporter` argument.

rspec-core/lib/rspec/core/example.rb
@@ -465,22 +465,22 @@ module RSpec
 
         if @exception
           execution_result.exception = @exception
-          record_finished :failed
+          record_finished :failed, reporter
           reporter.example_failed self
           false
         elsif pending_message
           execution_result.pending_message = pending_message
-          record_finished :pending
+          record_finished :pending, reporter
           reporter.example_pending self
           true
         else
-          record_finished :passed
+          record_finished :passed, reporter
           reporter.example_passed self
           true
         end
       end
 
-      def record_finished(status)
+      def record_finished(status, reporter)
         execution_result.record_finished(status, clock.now)
         reporter.example_finished(self)
       end

rspec-core/spec/rspec/core/example_spec.rb
@@ -574,52 +574,85 @@ RSpec.describe RSpec::Core::Example, :parent_metadata => 'sample' do
 
       reporter.register_listener(listener, :example_finished)
 
-      RSpec.describe do
-        it(&block)
-      end.run(reporter)
+      RSpec.describe(&block).run(reporter)
 
       reported_execution_result
     end
 
-    it "fills in the execution result details before reporting a passed example as finished" do
-      execution_result = capture_reported_execution_result_for_example do
-        expect(1).to eq 1
+    shared_examples "when skipped or failed" do
+      it "fills in the execution result details before reporting a failed example as finished" do
+        execution_result = capture_reported_execution_result_for_example do
+          expect(1).to eq 2
+        end
+
+        expect(execution_result).to have_attributes(
+          :status => :failed,
+          :exception => RSpec::Expectations::ExpectationNotMetError,
+          :finished_at => a_value_within(1).of(Time.now),
+          :run_time => a_value >= 0
+        )
       end
 
-      expect(execution_result).to have_attributes(
-        :status => :passed,
-        :exception => nil,
-        :finished_at => a_value_within(1).of(Time.now),
-        :run_time => a_value >= 0
-      )
+      it "fills in the execution result details before reporting a skipped example as finished" do
+        execution_result = capture_reported_execution_result_for_example do
+          skip "because"
+          expect(1).to eq 2
+        end
+
+        expect(execution_result).to have_attributes(
+          :status => :pending,
+          :pending_message => "because",
+          :finished_at => a_value_within(1).of(Time.now),
+          :run_time => a_value >= 0
+        )
+      end
     end
 
-    it "fills in the execution result details before reporting a failed example as finished" do
-      execution_result = capture_reported_execution_result_for_example do
-        expect(1).to eq 2
+    context "from an example" do
+      def capture_reported_execution_result_for_example(&block)
+        super { it(&block) }
       end
 
-      expect(execution_result).to have_attributes(
-        :status => :failed,
-        :exception => RSpec::Expectations::ExpectationNotMetError,
-        :finished_at => a_value_within(1).of(Time.now),
-        :run_time => a_value >= 0
-      )
+      it "fills in the execution result details before reporting a passed example as finished" do
+        execution_result = capture_reported_execution_result_for_example do
+          expect(1).to eq 1
+        end
+
+        expect(execution_result).to have_attributes(
+          :status => :passed,
+          :exception => nil,
+          :finished_at => a_value_within(1).of(Time.now),
+          :run_time => a_value >= 0
+        )
+      end
+
+      it "fills in the execution result details before reporting a pending example as finished" do
+        execution_result = capture_reported_execution_result_for_example do
+          pending "because"
+          expect(1).to eq 2
+        end
+
+        expect(execution_result).to have_attributes(
+          :status => :pending,
+          :pending_message => "because",
+          :pending_exception => RSpec::Expectations::ExpectationNotMetError,
+          :finished_at => a_value_within(1).of(Time.now),
+          :run_time => a_value >= 0
+        )
+      end
+
+      include_examples "when skipped or failed"
     end
 
-    it "fills in the execution result details before reporting a pending example as finished" do
-      execution_result = capture_reported_execution_result_for_example do
-        pending "because"
-        expect(1).to eq 2
+    context "from a context hook" do
+      def capture_reported_execution_result_for_example(&block)
+        super do
+          before(:context, &block)
+          it { will_never_run }
+        end
       end
 
-      expect(execution_result).to have_attributes(
-        :status => :pending,
-        :pending_message => "because",
-        :pending_exception => RSpec::Expectations::ExpectationNotMetError,
-        :finished_at => a_value_within(1).of(Time.now),
-        :run_time => a_value >= 0
-      )
+      include_examples "when skipped or failed"
     end
   end
 