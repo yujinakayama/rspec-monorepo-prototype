c9d5b0c9bba72f3d9ea8070b18b6b1e246b23481
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2019-03-04 10:13:38 +0200

    [support] Recognize hash as optional arg when optional keyword is present
    Given a method foo(a, b = {}, x: false) it should recognize that calling
    foo(1, 'a' => 1, 'b' => 2) is a valid method call.
    
    In case when the method has optional arguments and keyword arguments the
    ruby interpreter will treat the last Hash as keyword arguments if it
    contains Symbol keys. In the example above calling foo(1, a: 1) will
    raise an ArgumentError.

--- rspec-support/lib/rspec/support/spec/shell_out.rb
+++ rspec-support/lib/rspec/support/spec/shell_out.rb
@@ -59,15 +59,10 @@ module RSpec
           l =~ %r{bundler/source/rubygems} ||
           # Ignore bundler + rubygems warning.
           l =~ %r{site_ruby/\d\.\d\.\d/rubygems} ||
-          l =~ %r{jruby-\d\.\d\.\d\.\d/lib/ruby/stdlib/rubygems} ||
           # This is required for windows for some reason
           l =~ %r{lib/bundler/rubygems} ||
           # This is a JRuby file that generates warnings on 9.0.3.0
-          l =~ %r{lib/ruby/stdlib/jar} ||
-          # This is a JRuby file that generates warnings on 9.1.7.0
-          l =~ %r{org/jruby/RubyKernel\.java} ||
-          # Remove blank lines
-          l == "" || l.nil?
+          l =~ %r{lib/ruby/stdlib/jar}
         end.join("\n")
       end
 
